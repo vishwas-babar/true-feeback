2794a89da9ea9fc99c1ce5671f6a3a27cc07215a
vishwas babar <vishwasbabar886@gmail.com>
2024-04-18 16:57:48 +0530

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex mx-auto w-full">
+      <h1 class >This is the Home page</h1>
+    </div>
+  );
+}
